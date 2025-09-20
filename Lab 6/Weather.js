f0dfcbdc061a01b199be45c8dba5b3ea143e4ff3
Mohd Asad <asadmohd543@gmail.com>
2025-09-20 18:54:16 +0530

    Add files via upload

---
+++ Lab 6/Weather.js
@@ -0,0 +1,29 @@
+function checkWeather() {
+  var pincode = document.getElementById("pincode").value;
+  var apiKey = "b9cda8de934fba788999e5ebc2746c2a";
+  var url = `https://api.openweathermap.org/data/2.5/weather?zip=${pincode},IN&units=metric&appid=${apiKey}`;
+
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, true);
+
+  xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        const data = JSON.parse(this.responseText);
+        const city = data.name;
+        const temp = data.main.temp;
+
+        document.getElementById(
+          "result"
+        ).innerHTML = `<b>City:</b> ${city} <br> <b>Temperature:</b> ${temp} °C`;
+      } else {
+        document.getElementById(
+          "result"
+        ).innerHTML = `<span style="color:rgb(249, 52, 52); background-color: rgb(240, 232, 232);">
+     Error: Invalid Pincode or API issue
+   </span>`;
+      }
+    }
+  };
+  xmlHttp.send();
+}
